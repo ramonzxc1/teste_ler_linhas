719f5db0af9ea19be56cfa6699ee61f88c700b81
ramonzxc1 <ramonalternativeacc@gmail.com>
2023-06-08 14:43:51 -0300

    pequenas mudancas no print

--- main.cpp
+++ main.cpp
@@ -10,34 +10,34 @@ int main()
     string linha;
     while(getline(arquivo, linha))
     {
-        cout << "Linha antes do teste: " << linha << endl;
+        cout << "\nLinha a ser testada: " << linha << endl;
 
         if(linha == "carregarEmpresa()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "carregarAsg()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "carregarVendedor()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "carregarGerente()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "carregaDono()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "imprimeAsgs()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "imprimeVendedores()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "imprimeGerentes()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "imprimeDono()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "buscaFuncionario()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "calculaSalarioFuncionario()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "calculaTodoOsSalarios()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
         if(linha == "calcularRecisao()")
-            cout << "O teste com: '" << linha << "' funcionou.\n\n";
+            cout << "O teste com '" << linha << "' funcionou.\n";
     }
     arquivo.close();
     return 0;
